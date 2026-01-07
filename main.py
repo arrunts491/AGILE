1a3919c9af9ee1b0ec2ab45d868aae46313eb7a5
ARRUN T S <arrunts491@gmail.com>
2026-01-07 22:25:13 +0530

    Added login feature

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-print("HAPPY")
-print("HELLO")
+print("LIBRARY MANAGEMENT SYSTEM")
+print("10 NEW REGISTRATIONS")
 
 def login(username, password):
     if username == "admin" and password == "1234":
         print("Login successful")
     else:
         print("Invalid credentials")
+
