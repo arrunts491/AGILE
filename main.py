5bf9e9976d1ff0df5d7f13852d99d9ec6322c7c2
ARRUN T S <arrunts491@gmail.com>
2026-01-07 21:33:34 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-<<<<<<< HEAD
-print("HAPPPPPPPY")
-print("HELLO")
-=======
-print("HAPPY")
-print("HELLLLLLLO")
->>>>>>> conflict-branch
-
-def login(username, password):
-    if username == "admin" and password == "1234":
-        print("Login successful")
-    else:
-        print("Invalid credentials")
+print("LIBRARY MANAGEMENT SYSTEM")
