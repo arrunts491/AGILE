91f6eaf060f5ae9db48081a6edca02cda0248413
Sanjai <sanjaik@gmail.com>
2026-01-08 20:12:45 +0530

    Question 4: ATM withdrawal logic updated

---
+++ ATM.py
@@ -0,0 +1,15 @@
+# ATM Withdrawal System
+
+balance = 10000  # initial balance
+
+withdraw = int(input("Enter withdrawal amount: "))
+
+if withdraw % 100 != 0:
+    print("Amount must be a multiple of 100")
+elif withdraw > balance:
+    print("Insufficient balance")
+else:
+    balance = balance - withdraw
+    print("Withdrawal successful")
+    print("Updated balance:", balance)
+print("Changes in ATM Code")
